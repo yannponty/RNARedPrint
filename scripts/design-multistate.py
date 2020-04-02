2e76f4e21d37d9efce41ea00639c3e06abc1341f
Sebastian Will <swill@csail.mit.edu>
2020-04-02 15:07:38 +0200

    Add option '--energy' for multistate script

--- scripts/design-multistate.py
+++ scripts/design-multistate.py
@@ -87,6 +87,7 @@ def main():
     parser.add_argument("-T", "--temperature", type=float, default=37.0, help='Temperature of the energy calculations.')
     parser.add_argument("-n", "--number", type=int, default=1000, help='Number of designs to generate')
     parser.add_argument("-m", "--model", type=str, default='basepairs', help='Model for getting a new sequence: uniform, nussinov, basepairs, stacking')
+    parser.add_argument("-e", "--energy", type=float, default=None, help='Target energy')
     parser.add_argument("-g", "--gc", type=float, default=0.5, help='Target GC content.')
     parser.add_argument("-t", "--simple_tolerance", type=float,
             default=0.30, help='Tolerated relative deviation of simple energies.')
@@ -136,13 +137,17 @@ def main():
         stacksize = 1000
     sampler = RPSampler(structures, model=args.model, temperature=args.temperature, stacksize=stacksize, StopConstruct=True, debug=args.debug)
     construction_time = sampler.construction_time
+
     # get first sequence sample with energies with GC weight optimization and fixed high weights
     initialsample = take(1000,
                          Sample(sampler, nstr, target_energies=None,
                                 target_GC=args.gc, args=args))
 
     # get target energies
-    target_energies = getTargetEnergy(structures, initialsample, args)
+    if args.energy is None:
+        target_energies = getTargetEnergy(structures, initialsample, args)
+    else:
+        target_energies = [args.energy]*nstr
 
     target_turner_energies = target_energies[:]
 
